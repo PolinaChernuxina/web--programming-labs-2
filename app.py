2ea1a5d932f93bc9eff34c3251da23aaad45babd
Полина Чернухина <polinachernuxina@gmail.com>
2024-09-07 14:31:17 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Чернухина Полина Валерьевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h>web-сервер на flask</h>
+
+        <footer>
+            &copy; Чернухина Полина, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""

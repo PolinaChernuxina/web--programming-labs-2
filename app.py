7daf00756b0114c6c1ed184032aa9dab22df83e5
Полина Чернухина <polinachernuxina@gmail.com>
2024-09-07 15:05:30 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,35 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <nav>
+            <ul>
+                <li><a href="/lab1">Первая лабораторная</a></li>
+            </ul>
+        </nav>
+
+        <footer>
+            &copy; Чернухина Полина, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -16,6 +44,11 @@ def start():
         </header>
 
         <h>web-сервер на flask</h>
+
+        <p>Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</p>
 
         <footer>
             &copy; Чернухина Полина, ФБИ-23, 3 курс, 2024

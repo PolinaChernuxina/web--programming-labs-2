083cec7daac25c12858aeb1caf6cb8da5a88571f
Polina Chernuxina <polinachernuxina@gmail.com>
2024-12-27 23:28:08 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, redirect, url_for, render_template
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from urllib.parse import quote
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -8,11 +12,36 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
+
+from db.models import users
+from flask_login import LoginManager
 
 app = Flask (__name__)
 
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(login_id):
+    return users.query.get(int(login_id))
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_user = 'polina_chernuxina_own'
+    db_password = '123'
+    db_name = 'polina_chernuxina_own'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "polina_chernuxina_own.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{db_path}'
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -22,6 +51,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 @app.route("/index")
@@ -49,8 +79,10 @@ def menu():
     <li><a href="http://127.0.0.1:5000/lab5">Пятая лабораторная</a></li>  
     <li><a href="http://127.0.0.1:5000/lab6">Шестая лабораторная</a></li> 
     <li><a href="http://127.0.0.1:5000/lab7">Седьмая лабораторная</a></li> 
+    <li><a href="/lab8">Восьмая лабораторная</a></li>  
+    <li><a href="/lab9">Девятая лабораторная</a></li> 
     <footer>
-        &copy: Чернухина Полина, ФБИ-23, 3 курс, 2024
+        &copy: Бархатова Ольга, ФБИ-24, 3 курс, 2024
     </footer>
 </body>
 </html>
@@ -71,7 +103,7 @@ def not_found_404(err):
         </header>
         <h2>Ошибка 404 - такой страницы не существует</h2>
         <footer>
-            &copy; Чернухина Полина, ФБИ-23, 3 курс, 2024
+            &copy; Бархатова Ольга, ФБИ-24, 3 курс, 2024
         </footer>
     </body>
 </html>
@@ -93,7 +125,7 @@ def not_found_500(err):
         </header>
         <h2>Ошибка 500 - сервер не смог обработать запрос</h2>
         <footer>
-            &copy; Чернухина Полина, ФБИ-23, 3 курс, 2024
+            &copy; Бархатова Ольга, ФБИ-24, 3 курс, 2024
         </footer>
     </body>
 </html>
